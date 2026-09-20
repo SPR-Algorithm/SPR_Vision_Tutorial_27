c5d8aa5bd4ff792f68fa75db6b1dffdddd2b0d43
TWXFZFB <2397069774@qq.com>
2026-09-20 14:00:24 +0800

    feat:Vector_homework1

---
+++ homework1/include/Vector2.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include<vector>
+namespace rm{
+    using Vector = std::vector<double>
+    using Matrix = std::vector<std::vector<double>>; 
+    
+    double length(const Vector& l);
+    double distance(const Vector& a,const Vector& b);
+    double dot(const Vector& a,const Vector& b);
+    Vector scale(const Vector& l,double k);
+    Vector normalize(const Vector& l);
+    double angleBetween(const Vector& a,const Vector& b);
+}
